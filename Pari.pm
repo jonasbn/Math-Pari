e15f36b452863b1f84d538b66f6f7bbe23eb55f2
jonasbn <jonasbn@gmail.com>
2018-08-06 16:47:20 +0200

    Imported 2.0305_010810, ref: https://metacpan.org/release/ILYAZ/Math-Pari-2.0305_010810

--- Pari.pm
+++ Pari.pm
@@ -995,7 +995,7 @@ sub _shiftr {
 $initmem ||= 4000000;		# How much memory for the stack
 $initprimes ||= 500000;		# Calculate primes up to this number
 
-$VERSION = '2.010810';
+$VERSION = '2.0305_010810';
 
 my $true = 1;
 # Propagate sv_true, sv_false to SvIOK:
